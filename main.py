70a84503fa1b85afb87a6570de1522c0551e2bef
senagab <senagab@gmail.com>
2024-02-08 11:40:41 -0400

    feat: translation to english

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 import qrcode
 
-# url/texto
-dados_para_qrcode = "insira aqui o endereço do site, drive, imagem"
+# url/text
+qrcode_data = "Insert the website address, drive or image here"
 
-# cria objeto
+# creates object
 qr = qrcode.QRCode(
-    version=1,  # tamanho do qr code (quanto maior, mais informação)
+    version=1,  # qr code size (the bigger the size, the more information)
     error_correction=qrcode.constants.ERROR_CORRECT_H,  # troubleshooting
     box_size=10,
     border=4,
 )
 
 # input
-qr.add_data(dados_para_qrcode)
+qr.add_data(qrcode_data)
 qr.make(fit=True)
 
 # render
 img = qr.make_image(fill_color="white", back_color="#252525")
 
 # export
-img.save("bascuare-invert.png")
+img.save("filename.png")
